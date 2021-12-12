c10826fd350e963e6c3ae2a8cab0f94cd5af7b72
inventivetalent <mail@inventivetalent.org>
2021-12-12 11:55:18 +0100

    day4 2

--- src/main/java/org/inventivetalent/aoc/Day4.java
+++ src/main/java/org/inventivetalent/aoc/Day4.java
@@ -3,6 +3,7 @@ package org.inventivetalent.aoc;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Day4 extends Day {
@@ -12,7 +13,7 @@ public class Day4 extends Day {
     }
 
     BingoInput parseInput(String input) {
-        List<String> lines = new java.util.ArrayList<>(asLines(input));
+        List<String> lines = new ArrayList<>(asLines(input));
         BingoInput bingoInput = new BingoInput();
 
         String firstLine = lines.remove(0);
@@ -33,13 +34,23 @@ public class Day4 extends Day {
                 }
 
                 currentBoard = new BingoBoard();
-                currentBoard.rows = new int[5][5];
+                currentBoard.rows = new BingoField[5][5];
                 lineIndex = 0;
                 continue;
             }
 
             System.out.println(line);
-            int[] row = Arrays.stream(line.split(" ")).map(String::trim).filter(s -> !s.isBlank()).mapToInt(Integer::parseInt).toArray();
+            BingoField[] row = Arrays
+                    .stream(line.split(" "))
+                    .map(String::trim)
+                    .filter(s -> !s.isBlank())
+                    .map(i -> {
+                        BingoField field = new BingoField();
+                        field.value = Integer.parseInt(i);
+                        return field;
+                    })
+                    .toArray(BingoField[]::new);
+            System.out.println(Arrays.toString(row));
             assert currentBoard != null;
             currentBoard.rows[lineIndex] = row;
             lineIndex++;
@@ -48,9 +59,9 @@ public class Day4 extends Day {
         return bingoInput;
     }
 
-    int[][] rowsToCols(int[][] rows) {
-        // thanks, copilot
-        int[][] cols = new int[5][5];
+    BingoField[][] rowsToCols(BingoField[][] rows) {
+        // thanks, copilot!
+        BingoField[][] cols = new BingoField[5][5];
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 cols[j][i] = rows[i][j];
@@ -59,13 +70,13 @@ public class Day4 extends Day {
         return cols;
     }
 
-    boolean check(int[] arr, List<Integer> numbers) {
-        for (int i : arr) {
-            if (!numbers.contains(i)) {
-                return false;
-            }
+    boolean check(BingoField[] arr, int n) {
+        boolean r = true;
+        for (BingoField i : arr) {
+            if (i.value == n) i.marked = true;
+            if (!i.marked) r = false;
         }
-        return true;
+        return r;
     }
 
     @Override
@@ -81,7 +92,7 @@ public class Day4 extends Day {
         int lastDrawn = -1;
 
         BingoBoard winnerBoard = null;
-        int[] winnerRowOrCol = null;
+        BingoField[] winnerRowOrCol = null;
 
         game:
         while (!numbers.isEmpty()) {
@@ -90,16 +101,16 @@ public class Day4 extends Day {
             drawnNumbers.add(n);
 
             for (BingoBoard b : bingoInput.boards) {
-                for (int[] row : b.rows) {
-                    if (check(row, drawnNumbers)) {
+                for (BingoField[] row : b.rows) {
+                    if (check(row, n)) {
                         System.out.println("we have a winner row! " + Arrays.toString(row));
                         winnerBoard = b;
                         winnerRowOrCol = row;
                         break game;
                     }
                 }
-                for (int[] col : b.cols) {
-                    if (check(col, drawnNumbers)) {
+                for (BingoField[] col : b.cols) {
+                    if (check(col, n)) {
                         System.out.println("we have a winner col! " + Arrays.toString(col));
                         winnerBoard = b;
                         winnerRowOrCol = col;
@@ -116,11 +127,10 @@ public class Day4 extends Day {
             winnerBoard.print();
 
             List<Integer> unmarkedNumbersOnBoard = new ArrayList<>();
-            for (int[] arr : winnerBoard.rows) {
-                for (int i : arr) {
-                    if (!drawnNumbers.contains(i)) {
-                        unmarkedNumbersOnBoard.add(i);
-                    }
+            for (BingoField[] arr : winnerBoard.rows) {
+                for (BingoField i : arr) {
+                    if (i.marked) continue;
+                    unmarkedNumbersOnBoard.add(i.value);
                 }
             }
             int unmarkedSum = unmarkedNumbersOnBoard.stream().mapToInt(i -> i).sum();
@@ -135,6 +145,76 @@ public class Day4 extends Day {
 
     @Override
     public Object run2(String input) {
+        BingoInput bingoInput = parseInput(input);
+        System.out.println(bingoInput.input);
+        bingoInput.boards.forEach(b -> {
+            b.print();
+        });
+
+        List<Integer> numbers = new ArrayList<>(bingoInput.input);
+        List<Integer> drawnNumbers = new ArrayList<>();
+        int lastDrawn = -1;
+        int lastWinnerDraw = -1;
+
+        BingoBoard winnerBoard = null;
+        BingoField[] winnerRowOrCol = null;
+
+        game:
+        while (!numbers.isEmpty()) {
+            int n = numbers.remove(0);
+            lastDrawn = n;
+            drawnNumbers.add(n);
+
+
+            boards:
+            for (BingoBoard b : bingoInput.boards) {
+                if (b.won) continue;
+                for (BingoField[] row : b.rows) {
+                    if (check(row, n)) {
+                        System.out.println("we have a winner row! " + Arrays.toString(row));
+                        winnerBoard = b;
+                        winnerRowOrCol = row;
+                        b.won = true;
+
+                        lastWinnerDraw = lastDrawn;
+                        continue boards;
+                    }
+                }
+                for (BingoField[] col : b.cols) {
+                    if (check(col, n)) {
+                        System.out.println("we have a winner col! " + Arrays.toString(col));
+                        winnerBoard = b;
+                        winnerRowOrCol = col;
+                        b.won = true;
+
+                        lastWinnerDraw = lastDrawn;
+                        continue boards;
+                    }
+                }
+            }
+
+        }
+
+        if (winnerBoard == null) {
+            System.out.println("no winner :(");
+        } else {
+            System.out.println("winner board");
+            winnerBoard.print();
+
+            List<Integer> unmarkedNumbersOnBoard = new ArrayList<>();
+            for (BingoField[] arr : winnerBoard.rows) {
+                for (BingoField i : arr) {
+                    if (i.marked) continue;
+                    unmarkedNumbersOnBoard.add(i.value);
+                }
+            }
+            int unmarkedSum = unmarkedNumbersOnBoard.stream().mapToInt(i -> i).sum();
+            System.out.println("unmarked sum: " + unmarkedSum);
+            System.out.println("last winner draw: " + lastWinnerDraw);
+
+            return unmarkedSum * lastWinnerDraw;
+        }
+
         return null;
     }
 
@@ -159,15 +239,16 @@ public class Day4 extends Day {
 
     class BingoBoard {
 
-        int[][] rows;
-        int[][] cols;
+        BingoField[][] rows;
+        BingoField[][] cols;
+        boolean won;
 
         void print() {
-            for (int[] a : rows) {
+            for (BingoField[] a : rows) {
                 System.out.println(Arrays.toString(a));
             }
             System.out.println(" ");
-            for (int[] a : cols) {
+            for (BingoField[] a : cols) {
                 System.out.println(Arrays.toString(a));
             }
             System.out.println(" ");
@@ -175,10 +256,50 @@ public class Day4 extends Day {
         }
 
         @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            BingoBoard that = (BingoBoard) o;
+            return Arrays.deepEquals(rows, that.rows) && Arrays.deepEquals(cols, that.cols);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = Arrays.deepHashCode(rows);
+            result = 31 * result + Arrays.deepHashCode(cols);
+            return result;
+        }
+
+        @Override
         public String toString() {
             return "BingoBoard{" +
                     "board=" + Arrays.toString(rows) +
                     '}';
+        }
+
+    }
+
+    class BingoField {
+
+        int value;
+        boolean marked;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            BingoField that = (BingoField) o;
+            return value == that.value && marked == that.marked;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(value, marked);
+        }
+
+        @Override
+        public String toString() {
+            return "[" + (marked ? "X" : " ") + "] " + value;
         }
 
     }
