dc0d08b5543c51dab38ccb99b5322d10bb86fb69
Haylee Schäfer <mail@inventivetalent>
2021-12-14 21:40:10 +0100

    day6 2

--- src/main/java/org/inventivetalent/aoc/Day6.java
+++ src/main/java/org/inventivetalent/aoc/Day6.java
@@ -10,38 +10,70 @@ public class Day6 extends Day {
         super();
     }
 
-
-    @Override
-    public Object run1(String input) {
-        var originalTimers = Arrays.stream(input.split(",")).map(Integer::parseInt).collect(Collectors.toList());
-        var timers = new ArrayList<>(originalTimers);
+    // naive solution, just takes like a week to solve lol
+    int run(String input, int days) {
+        var timers = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
 
         System.out.println("Initial: " + timers);
 
-        for (int day = 0; day < 80; day++) {
+        for (int day = 0; day < days; day++) {
 
-            int size = timers.size();
+            int size = timers.length;
             for (int i = 0; i < size; i++) {
-                int timer = timers.get(i);
+                int timer = timers[i];
                 if (timer == 0) {
                     timer = 6;
-                    timers.add(8);
+
+                    int[] newTimers = new int[timers.length + 1];
+                    System.arraycopy(timers, 0, newTimers, 0, timers.length);
+                    newTimers[newTimers.length - 1] = 8;
+                    timers = newTimers;
                 } else {
                     timer--;
                 }
-                timers.set(i, timer);
+                timers[i] = timer;
             }
 
-            System.out.println("After " + (day + 1) + " days: " + timers.size()/*+" " + timers*/);
+            System.out.println("After " + (day + 1) + " days: " + timers.length/*+" " + timers*/);
 
         }
 
-        return timers.size();
+        return timers.length;
+    }
+
+    // https://www.reddit.com/r/adventofcode/comments/r9z49j/2021_day_6_solutions/
+    long runBuckets(String input, int days) {
+        int[] timers = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
+        long[] counts = new long[10];
+        for (int timer : timers) {
+            counts[timer]++;
+        }
+        System.out.println(Arrays.toString(counts));
+
+        for (int day = 0; day < days; day++) {
+            counts[7] += counts[0];
+            counts[9] = counts[0];
+            for (int i = 0; i < 9; i++) {
+                counts[i] = counts[i + 1];
+            }
+            counts[9] = 0;
+
+            //System.out.println(Arrays.toString(counts));
+        }
+
+        System.out.println(Arrays.toString(counts));
+
+        return Arrays.stream(counts).sum();
+    }
+
+    @Override
+    public Object run1(String input) {
+        return runBuckets(input, 80);
     }
 
     @Override
     public Object run2(String input) {
-        return null;
+        return runBuckets(input, 256);
     }
 
     public static void main(String[] args) {
