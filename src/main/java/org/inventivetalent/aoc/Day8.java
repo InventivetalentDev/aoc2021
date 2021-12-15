44908b1ba6fb2383b729cbf45ed8c995a4482842
inventivetalent <mail@inventivetalent.org>
2021-12-15 19:12:14 +0100

    .

---
+++ src/main/java/org/inventivetalent/aoc/Day8.java
@@ -0,0 +1,23 @@
+package org.inventivetalent.aoc;
+
+public class Day8 extends Day {
+
+    public Day8() {
+        super();
+    }
+
+    @Override
+    public Object run1(String input) {
+        return null;
+    }
+
+    @Override
+    public Object run2(String input) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        new Day8();
+    }
+
+}
