646cdf3db67fb50de8d4f444e0e20f5faf6967a4
inventivetalent <mail@inventivetalent.org>
2021-12-15 09:27:02 +0100

    day7 2

--- src/main/java/org/inventivetalent/aoc/Day7.java
+++ src/main/java/org/inventivetalent/aoc/Day7.java
@@ -1,7 +1,9 @@
 package org.inventivetalent.aoc;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 public class Day7 extends Day {
@@ -14,23 +16,33 @@ public class Day7 extends Day {
         return Math.abs(targetPosition - position);
     }
 
-    int costToAlignTo(List<Integer> positions, int targetPosition) {
+
+    int addedCostToAlignTo(int position, int targetPosition) {
+        int diff = Math.abs(targetPosition - position);
+        // 1+2+3+4+...+n = n*(n+1)/2
+        return (diff * (diff + 1)) / 2;
+    }
+
+    int costToAlignTo(List<Integer> positions, int targetPosition, BiFunction<Integer, Integer, Integer> costFunction) {
         int totalCost = 0;
         for (int pos : positions) {
-            totalCost += costToAlignTo(pos, targetPosition);
+            int c = costFunction.apply(pos, targetPosition);
+//            System.out.println(pos + "->" + targetPosition + " = " + c);
+            totalCost += c;
         }
         return totalCost;
     }
 
-
-    @Override
-    public Object run1(String input) {
+    int run(String input, BiFunction<Integer, Integer, Integer> costFunction) {
         var positions = Arrays.stream(input.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+
+        int minPosition = positions.stream().min(Comparator.naturalOrder()).orElseThrow();
+        int maxPosition = positions.stream().max(Comparator.naturalOrder()).orElseThrow();
 
         int bestCost = Integer.MAX_VALUE;
         int bestPosition = 0;
-        for (int position : positions) {
-            int cost = costToAlignTo(positions, position);
+        for (int position = minPosition; position <= maxPosition; position++) {
+            int cost = costToAlignTo(positions, position, costFunction);
             if (cost < bestCost) {
                 bestCost = cost;
                 bestPosition = position;
@@ -43,8 +55,13 @@ public class Day7 extends Day {
     }
 
     @Override
+    public Object run1(String input) {
+        return run(input, this::costToAlignTo);
+    }
+
+    @Override
     public Object run2(String input) {
-        return null;
+        return run(input, this::addedCostToAlignTo);
     }
 
     public static void main(String[] args) {
