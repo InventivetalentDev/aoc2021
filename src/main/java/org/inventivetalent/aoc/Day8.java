831ee4892a91854cd18f2d061b7a6a2054e86e6c
Haylee Schäfer <mail@inventivetalent>
2021-12-21 20:03:04 +0100

    .

--- src/main/java/org/inventivetalent/aoc/Day8.java
+++ src/main/java/org/inventivetalent/aoc/Day8.java
@@ -1,6 +1,23 @@
 package org.inventivetalent.aoc;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Day8 extends Day {
+
+    static final String[] NUMBER_PATTERNS = new String[]{
+            "abcefg",  // 0
+            "cf",      // 1
+            "acdeg",   // 2
+            "acdfg",   // 3
+            "bcdf",    // 4
+            "abdfg",   // 5
+            "abdefg",  // 6
+            "acf",     // 7
+            "abcdefg", // 8
+            "abcdfg"   // 9
+    };
+
 
     public Day8() {
         super();
@@ -8,11 +25,13 @@ public class Day8 extends Day {
 
     @Override
     public Object run1(String input) {
+        //TODO
         return null;
     }
 
     @Override
     public Object run2(String input) {
+        //TODO
         return null;
     }
 
